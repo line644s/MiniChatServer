a320b61e3a33a78d104d69e858fdb65dbe36df0b
Line Phoebe Wienke <line644s@edu.easj.dk>
2019-08-29 11:38:21 +0200

    Add project files.

---
+++ MiniChatServer/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace MiniChatServer
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
