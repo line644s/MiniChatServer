47c0a64d372672fd0fa8b96bf72fda5b97608aae
Line Phoebe Wienke <line644s@edu.easj.dk>
2019-08-31 23:08:57 +0200

    Opgave B Client

---
+++ Client/Client.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.IO;
+using System.Net.Sockets;
+
+namespace MiniChatClient
+{
+    internal class Client
+    {
+        public void Start()
+        {
+            TcpClient socket = new TcpClient("localhost", 7070);
+
+            Stream ns = socket.GetStream();
+            
+            using (StreamReader sr = new StreamReader(ns))
+            using (StreamWriter sw = new StreamWriter(ns))
+            {
+
+                sw.WriteLine(Console.ReadLine());
+                sw.Flush();
+
+                string line = sr.ReadLine();
+                Console.WriteLine(line);
+
+                //string str = Console.ReadLine();
+
+                //sw.WriteLine(str);
+                //sw.Flush();
+
+                //string strin = sr.ReadLine();
+                //Console.WriteLine(strin);
+            }
+        }
+    }
+}
---
+++ Client/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace MiniChatClient
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Client client = new Client();
+            client.Start();
+
+            Console.ReadLine();
+        }
+    }
+}
